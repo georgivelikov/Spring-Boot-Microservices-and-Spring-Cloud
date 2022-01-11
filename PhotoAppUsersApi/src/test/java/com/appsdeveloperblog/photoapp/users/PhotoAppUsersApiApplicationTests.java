44e7f0e99dd403b82dd7794f56b31d6a7336b6f4
Georgi Velikov <gmvelikov@gmail.com>
2022-01-11 13:36:55 +0200

    Initial add

---
+++ PhotoAppUsersApi/src/test/java/com/appsdeveloperblog/photoapp/users/PhotoAppUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
