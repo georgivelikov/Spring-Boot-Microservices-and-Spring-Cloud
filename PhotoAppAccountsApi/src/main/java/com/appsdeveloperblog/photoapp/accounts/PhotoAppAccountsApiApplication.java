f8bf8d236e97e10fe7b9cdd9a6e4b6f2fc43be7e
Georgi Velikov <gmvelikov@gmail.com>
2022-01-11 14:16:37 +0200

    Innitial

---
+++ PhotoAppAccountsApi/src/main/java/com/appsdeveloperblog/photoapp/accounts/PhotoAppAccountsApiApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppAccountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppAccountsApiApplication.class, args);
+	}
+
+}
---
+++ PhotoAppAccountsApi/src/test/java/com/appsdeveloperblog/photoapp/accounts/PhotoAppAccountsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
