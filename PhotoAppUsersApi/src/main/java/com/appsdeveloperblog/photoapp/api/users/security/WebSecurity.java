c875d3695235b0d39840001fb7ca7780ec33b47a
Georgi Velikov <gmvelikov@gmail.com>
2022-01-16 18:58:41 +0200

    Adding url sec

--- PhotoAppUsersApi/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppUsersApi/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.appsdeveloperblog.photoapp.api.users.io.repository.UserRepository;
 import com.appsdeveloperblog.photoapp.api.users.service.UserService;
@@ -17,9 +23,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final UserRepository userRepository;
 
-    @Autowired
-    private Environment env;
-
     public WebSecurity(UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder,
 	    UserRepository userRepository) {
 	this.userDetailsService = userDetailsService;
@@ -30,12 +33,62 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 	// Needed for h2
-	http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/h2-console/**").permitAll();
+	http.authorizeRequests()
+		.antMatchers("/")
+		.permitAll()
+		.antMatchers("/h2-console/**")
+		.permitAll();
 	// Needed for h2
-	http.headers().frameOptions().disable();
-	http.csrf().disable();
-	http.authorizeRequests().antMatchers("/users/**").permitAll();
+	http.headers()
+		.frameOptions()
+		.disable();
+	http.cors()
+		.and()
+		.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+		.permitAll()
+		.antMatchers(HttpMethod.GET, "/users/status/check")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.addFilter(getAuthenticationFilter("/users/login"))
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);// No session, no cookies
 
     }
 
+    // Spring default login url is '/login'. You could change it by adding
+    // custom
+    // url
+    public AuthenticationFilter getAuthenticationFilter(String url) throws Exception {
+	final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+	if (url != null) {
+	    filter.setFilterProcessesUrl(url);
+	}
+
+	return filter;
+    }
+
+    // Returns AuthenticationFilter with Spring default login url '/login'
+    public AuthenticationFilter getDefaultAuthenticationFilter() throws Exception {
+	return getAuthenticationFilter(null);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+	final CorsConfiguration configuration = new CorsConfiguration();
+
+	configuration.setAllowedOrigins(Arrays.asList("*"));
+	configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD"));
+	configuration.setAllowCredentials(true);
+	configuration.setAllowedHeaders(Arrays.asList("*"));
+
+	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source.registerCorsConfiguration("/**", configuration);
+
+	return source;
+    }
 }
