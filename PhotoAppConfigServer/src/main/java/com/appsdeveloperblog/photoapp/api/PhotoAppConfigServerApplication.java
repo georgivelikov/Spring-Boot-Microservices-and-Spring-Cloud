1952b346eb3020db5c6da7e44d4c8f5d2a8227b8
Georgi Velikov <gmvelikov@gmail.com>
2022-01-24 21:44:22 +0200

    Adding configs

--- PhotoAppConfigServer/src/main/java/com/appsdeveloperblog/photoapp/api/PhotoAppConfigServerApplication.java
+++ PhotoAppConfigServer/src/main/java/com/appsdeveloperblog/photoapp/api/PhotoAppConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.appsdeveloperblog.photoapp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PhotoAppConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(PhotoAppConfigServerApplication.class, args);
+    }
 
 }
